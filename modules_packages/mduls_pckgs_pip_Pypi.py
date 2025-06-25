83997a5fd93d04f5e0119527f3be587d28ae9147
Vikasrao1 <tvikasrao1@gmail.com>
2025-06-25 13:10:05 -0500

    create new folder

---
+++ modules_packages/mduls_pckgs_pip_Pypi.py
@@ -0,0 +1,6 @@
+#77 pip install and PyPi
+#PyPi is a repository for open-source third party packages.
+# to install packages using pip goto terminal (ex: pip install requests/pip install colorama 
+# (colorama allows us to incorporate color))
+
+#78 Modules and Packages( How to write our modules and packages)
